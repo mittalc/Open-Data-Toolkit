20336d259715992ba600c1d50e43e5d65107412f
Harsaroop Singh <harsaroop.singh0530@gmail.com>
2020-07-15 17:54:05 +0530

    Update GUI.java

--- Source Code/GUI.java
+++ Source Code/GUI.java
@@ -1,1 +1,18 @@
 // This is class for the GUI of application
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+
+public class GUI extends JPanel{
+  
+  public void paint(Graphics g){
+    // this method does not returrn something but it sets the paint or background colour of frame
+  }
+  
+  public void display_data(Graphics g){
+    // this class does not return something but it displays the data api asks for
+  }
+  
+}
