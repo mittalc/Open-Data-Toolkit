9b5ee2f5925a5595a5dc514840a0f42f9e33ac88
Daljeet Singh <61301173+Daljeet-sandhu@users.noreply.github.com>
2020-07-15 12:38:25 -0400

    Update theme.java

--- Source Code/Features/theme.java
+++ Source Code/Features/theme.java
@@ -1,1 +1,31 @@
-//for themes
+// This is the theme class that contains the theme data
+public class theme{
+  public static void main(String[] args)
+  {
+    private String theme;
+    private String theme_data;
+    private String theme_info;
+  
+    
+    public String add_theme(){
+      // this function adds the themes
+      return String;
+    }
+    
+    public String theme_data(){
+      return String;                          //stores all the themes data
+    }
+    
+    public String get_theme_data(){
+      return String;                            //gets all the data
+    }
+    
+    public String remove_theme_data(){
+      return String;                           
+    }
+    
+     public String edit_theme_data(){
+      return String;                           
+     }
+  }                   
+}
