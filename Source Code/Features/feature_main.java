0de54c3aa6717084ebd3ec4a0b0fa00f8bdac126
Daljeet Singh <61301173+Daljeet-sandhu@users.noreply.github.com>
2020-07-15 12:31:14 -0400

    Update feature_main.java

--- Source Code/Features/feature_main.java
+++ Source Code/Features/feature_main.java
@@ -1,1 +1,38 @@
-//this class connects all the feature classes
+// This is the feature class that connects all other feature class
+public class feature_main{
+  public static void main(String[] args)
+  {
+    private String Data;
+    private int user_id;
+    private String feature;
+    private String info; 
+  
+    
+    public String feature(){
+      // this function gets the feature 
+      return String;
+    }
+    
+    public String feature_data(){
+      return String;                          //stores all the feature data
+    }
+    
+    public String get_feature_data(){
+      return String;                            //gets all the data
+    }
+    
+     public String connect(){
+      return String;                  
+    }
+    
+    public String service(){
+      return String;                           
+    }
+    
+    public String to_String(){
+      return String;                            
+    }
+  }
+                       
+                   
+}s
