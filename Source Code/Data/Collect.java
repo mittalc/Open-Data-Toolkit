b8c9be9ea3f06e268bb220e4e82544757889f8f3
Harsaroop Singh <harsaroop.singh0530@gmail.com>
2020-07-15 19:08:45 +0530

    Update Collect.java

--- Source Code/Data/Collect.java
+++ Source Code/Data/Collect.java
@@ -1,1 +1,48 @@
 // This is the class for collecting the data
+
+import java.util.*;
+
+public class Collect(){
+  private String data; // data attribute
+  private String user_info; // attribute for user information 
+  private String section_info; // attribute for section information
+  private String date_type; // attribute to store data_type
+  
+  // getter and setter methods
+  
+  public void setdata(String data){
+    this.data = data;
+  }
+  
+  public void setuser_info(String user_info){
+    this.user_info = user_info;
+  }
+  
+  public void setsection_info(String section_info){
+    this.section_info = section_info;
+  }
+  
+  public void setdata_type(String data_type){
+    this.data_type = data_type;
+  }
+  
+  public String getdata(){
+    return data;
+  }
+  
+  public String getuser_info(){
+    return user_info;
+  }
+  
+public String getsection_info(){
+    return section_info;
+  }
+  
+  public String getdata_type(){
+    return data_type;
+  }
+  
+  public void collect(){
+    // this method collects the data and store it into sql file or database
+  }
+}
