21310c0fa9e296cc9b629f76f8627f1dd93ef1d5
Daljeet Singh <61301173+Daljeet-sandhu@users.noreply.github.com>
2020-07-15 12:37:13 -0400

    Update plug_ins.java

--- Source Code/Features/plug_ins.java
+++ Source Code/Features/plug_ins.java
@@ -1,1 +1,31 @@
-// for all the plug ins used in this project
+// This is the media class that contains the media data
+public class media{
+  public static void main(String[] args)
+  {
+    private String plug_ins;
+    private String plug_ins_data;
+    private String plug_ins_info;
+  
+    
+    public String add_plug_ins(){
+      // this function adds the plug_ins
+      return String;
+    }
+    
+    public String plug_ins_data(){
+      return String;                          //stores all the plug_ins data
+    }
+    
+    public String get_plug_ins_data(){
+      return String;                            //gets all the data
+    }
+    
+    public String remove_plug_ins_data(){
+      return String;                           
+    }
+    
+     public String edit_plug_ins_data(){
+      return String;                           
+     }
+  }                   
+}
