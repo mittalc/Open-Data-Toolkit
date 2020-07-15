514e301828c9d25351be659f8a27d908e7c4d1ca
Daljeet Singh <61301173+Daljeet-sandhu@users.noreply.github.com>
2020-07-15 12:26:50 -0400

    Update help_user.java

--- Source Code/help_user.java
+++ Source Code/help_user.java
@@ -1,1 +1,40 @@
-//class to provide help to the users
+// This is the help class to help user with any complaint
+public class help_user{
+  public static void main(String[] args)
+  {
+    private String Data;
+    private int user_id;
+    private String complaint;
+    private String service; 
+      
+    System.out.println("Hello, How can we help you!");
+    System.out.println("The UI version of this software is comming soon");
+    
+    public String get_complaint(){
+      // this function returns the complaint from the user 
+      return complaint;
+    }
+    
+    public String service(){
+      return String;
+    }
+    
+    public String get_solution_data(){
+      return String;                             //give the help data related to the complaint
+    }
+    
+     public String give_solution(){
+      return String;                         //staff provides solution
+    }
+    
+    public String solution_data(){
+      return String;                             //stores all the help data
+    }
+    
+    public String emergency(){
+      return String;                             //contacts the staff
+    }
+  }
+                       
+                   
+}
