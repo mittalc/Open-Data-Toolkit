c35b24250d253c65e4888ad451684dbbbdc55661
Daljeet Singh <61301173+Daljeet-sandhu@users.noreply.github.com>
2020-07-15 12:35:23 -0400

    Update media.java

--- Source Code/Features/media.java
+++ Source Code/Features/media.java
@@ -1,1 +1,34 @@
-// for all the media used in this project
+// This is the media class that contains the media data
+public class media{
+  public static void main(String[] args)
+  {
+    private String media;
+    private String media_data;
+  
+    
+    public String add_media(){
+      // this function adds the media
+      return String;
+    }
+    
+    public String media_data(){
+      return String;                          //stores all the media data
+    }
+    
+    public String get_media_data(){
+      return String;                            //gets all the data
+    }
+    
+    public String remove_media_data(){
+      return String;                           
+    }
+    
+     public String edit_media_data(){
+      return String;                           
+    }
+    
+    public String to_String(){
+      return String;                            
+    }
+  }                   
+}
