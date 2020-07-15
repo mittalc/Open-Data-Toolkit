d9472129fd2c3d5523c7f00c169edf8d3c6dcfa3
Harsaroop Singh <harsaroop.singh0530@gmail.com>
2020-07-15 17:46:36 +0530

    Update Main.java

--- Source Code/Main.java
+++ Source Code/Main.java
@@ -1,1 +1,34 @@
 // This is the Main class or drivwer class for all other class
+public class Main{
+  public static void main(String[] args)
+  {
+    private UserInterface UI = new UserInterface();
+    private String Data;
+    private int user_id;
+    private String user_name;
+    private String user_info; 
+      
+    System.out.println("Hello, Welcome to the Open Data toolkit!");
+    System.out.println("The UI version of this software is comming soon");
+    
+    public String get_data(){
+      // this function returns the data the user or the API asks for 
+      return Data;
+    }
+    
+    public String section(){
+      return String;
+    }
+    
+     public String type(){
+      return String;
+    }
+    
+    public String user_account(){
+      return String;
+    }
+    
+  }
+                       
+                   
+}
