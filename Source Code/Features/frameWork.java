884ee042f9103eeb373bebb2127aed0eee7d445c
Daljeet Singh <61301173+Daljeet-sandhu@users.noreply.github.com>
2020-07-15 12:33:24 -0400

    Update frameWork.java

--- Source Code/Features/frameWork.java
+++ Source Code/Features/frameWork.java
@@ -1,1 +1,33 @@
-// for the frameworks of the websites
+// This is the frameWork class that contains the frameWork data
+public class frameWork{
+  public static void main(String[] args)
+  {
+    private String Data;
+    private int user_id;
+    private String frameWork;
+    private String frameWork_data;
+    private String info; 
+  
+    
+    public String set_frameWork(){
+      // this function sets the frameWork
+      return String;
+    }
+    
+    public String frameWork_data(){
+      return String;                          //stores all the frameWork data
+    }
+    
+    public String get_frameWork_data(){
+      return String;                            //gets all the data
+    }
+    
+    public String service(){
+      return String;                           
+    }
+    
+    public String to_String(){
+      return String;                            
+    }
+  }                   
+}
