cae5dee7be0492f2ff2098e25977860cf61c2701
Harsaroop Singh <harsaroop.singh0530@gmail.com>
2020-07-15 20:28:54 +0530

    Update Manage.java

--- Source Code/Data/Manage.java
+++ Source Code/Data/Manage.java
@@ -1,1 +1,28 @@
-// This is the class to manage the data
+// This is the class to manage the data and send it out to the user from database 
+
+import java.util.*;
+import java.sql.*;
+
+public class Manage(){
+  private String user_info;
+  private String data;
+  
+  // JDBC driver name and database URL
+  static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+  static final String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+
+  //  Database credentials
+  static final String USER = "username";
+  static final String PASS = "password";
+    
+  public void initate_database(){
+    // this is the method to open a connection with database and get the data out of it 
+    Connection conn = null;
+   Statement stmt = null;
+  }
+  
+  public String get_data(){
+    // method to return the extracted data to the user
+    return data;
+  }
+}
